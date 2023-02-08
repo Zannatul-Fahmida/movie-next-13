c4ba6bd9ddac6e969c28d8a04e485373edead548
zannatul <jannatulfahmida15@gmail.com>
2023-02-08 22:02:52 +0600

    dark mode added

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Montserrat } from "@next/font/google";
 import Navbar from "@/components/Navbar";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
 
 const montserrat = Montserrat({
   weight: ["400", "700"],
@@ -12,17 +13,19 @@ const montserrat = Montserrat({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <SessionProvider>
-        <head />
-        <body className={`${montserrat.className}`}>
-          <Navbar />
-          {children}
-        </body>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <head />
+          <body className={`${montserrat.className}`}>
+            <Navbar />
+            {children}
+          </body>
+        </ThemeProvider>
       </SessionProvider>
     </html>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,13 @@
 "use client";
 import Link from "next/link";
 import { FiMenu } from "react-icons/fi";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
 import { useSession, signOut } from "next-auth/react";
 import { usePathname } from "next/navigation";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+  const { theme, setTheme } = useTheme();
   const pathname = usePathname();
   const { data: session } = useSession();
   const handleNav = () => {
@@ -12,26 +15,14 @@ export default function Navbar() {
   };
   return (
     <header>
-      <nav
-        className="
-          flex flex-wrap
-          items-center
-          justify-between
-          w-full
-          py-4
-          md:py-0
-          px-4
-          text-lg text-gray-700
-          bg-white
-        "
-      >
+      <nav className="flex flex-wrap items-center justify-between w-full py-4 md:py-0 px-4 text-lg text-gray-700 bg-white dark:bg-black">
         <div>
-          <Link className="font-bold" href="/">
+          <Link className="font-bold dark:text-white" href="/">
             Movie<span className="text-rose-700">Next</span>
           </Link>
         </div>
         <button
-          className="h-6 w-6 cursor-pointer md:hidden block"
+          className="h-6 w-6 cursor-pointer md:hidden block dark:text-white"
           onClick={handleNav}
         >
           <FiMenu />
@@ -42,12 +33,7 @@ export default function Navbar() {
           id="menu"
         >
           <ul
-            className="
-              pt-4
-              text-base text-gray-700
-              md:flex
-              md:justify-between 
-              md:pt-0"
+            className="pt-4 text-base text-gray-700 md:flex md:justify-between md:pt-0 dark:text-white"
           >
             <li>
               <Link
@@ -117,6 +103,15 @@ export default function Navbar() {
                 </button>
               </li>
             )}
+            <button
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+              {theme === "dark" ? (
+                <BsMoonFill className="text-white w-5 h-5" />
+              ) : (
+                <BsSunFill className="text-yellow-500 w-5 h-5" />
+              )}
+            </button>
           </ul>
         </div>
       </nav>
